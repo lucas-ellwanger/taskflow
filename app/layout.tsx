cdacb9b8aa16ce8b1d46b66bf07f11670c7cc414
Lucas Ellwanger <lucasellwanger.dev@gmail.com>
2023-12-10 01:16:00 -0300

    move clerk provider to sub route

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import "@/styles/globals.css";
 import { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { cookies } from "next/headers";
-import { ClerkProvider } from "@clerk/nextjs";
 
 import { siteConfig } from "@/config/site";
 import { EdgeStoreProvider } from "@/lib/edgestore";
@@ -34,14 +33,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className={`font-sans antialiased ${inter.variable}`}>
-          <TRPCReactProvider cookies={cookies().toString()}>
-            <EdgeStoreProvider>{children}</EdgeStoreProvider>
-          </TRPCReactProvider>
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body className={`font-sans antialiased ${inter.variable}`}>
+        <TRPCReactProvider cookies={cookies().toString()}>
+          <EdgeStoreProvider>{children}</EdgeStoreProvider>
+        </TRPCReactProvider>
+      </body>
+    </html>
   );
 }
