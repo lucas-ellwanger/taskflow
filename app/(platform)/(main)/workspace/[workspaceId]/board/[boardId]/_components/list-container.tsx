be69c4e45c9a7f58cee2706dd32fa7e3780a7864
Lucas Ellwanger <lucasellwanger.dev@gmail.com>
2023-12-29 14:30:12 -0300

    ok

--- app/(platform)/(main)/workspace/[workspaceId]/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(main)/workspace/[workspaceId]/board/[boardId]/_components/list-container.tsx
@@ -13,15 +13,122 @@ interface ListContainerProps {
   data: ListWithCards[];
 }
 
+function reorder<T>(list: T[], startIndex: number, endIndex: number) {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  if (removed !== undefined) result.splice(endIndex, 0, removed);
+
+  return result;
+}
+
 export const ListContainer = ({ boardId, data }: ListContainerProps) => {
-  const [orderedLists, setOrderedLists] = useState(data);
+  const [orderedData, setOrderedData] = useState(data);
 
   useEffect(() => {
-    setOrderedLists(data);
+    setOrderedData(data);
   }, [data]);
 
+  const onDragEnd = (result: any) => {
+    const { destination, source, type } = result;
+
+    if (!destination) {
+      return;
+    }
+
+    // if dropped in the same position
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    // if user moves a list
+    if (type === "list") {
+      const items = reorder(orderedData, source.index, destination.index).map(
+        (item, index) => ({
+          ...item,
+          position: index,
+        })
+      );
+
+      setOrderedData(items);
+      // TODO: Trigger mutation
+    }
+
+    // if user moves a card
+    if (type === "card") {
+      let newOrderedData = [...orderedData];
+
+      // get source and destination list
+      const sourceList = newOrderedData.find(
+        (list) => list.id === source.droppableId
+      );
+      const destList = newOrderedData.find(
+        (list) => list.id === destination.droppableId
+      );
+
+      if (!sourceList || !destList) {
+        return;
+      }
+
+      // if cards exists in source list
+      if (!sourceList.cards) {
+        sourceList.cards = [];
+      }
+
+      // if cards exists in destination list
+      if (!destList.cards) {
+        destList.cards = [];
+      }
+
+      // if user moves a card in the same list
+      if (source.droppableId === destination.droppableId) {
+        const reorderedCards = reorder(
+          sourceList.cards,
+          source.index,
+          destination.index
+        );
+
+        reorderedCards.forEach((card, idx) => {
+          card.position = idx;
+        });
+
+        sourceList.cards = reorderedCards;
+
+        setOrderedData(newOrderedData);
+        // TODO: Trigger mutation
+        // if user moves a card to another list
+      } else {
+        // remove card from the source list
+        const [movedCard] = sourceList.cards.splice(source.index, 1);
+
+        if (!movedCard) {
+          return;
+        }
+
+        // assign the new listId to the moved card
+        movedCard.listId = destination.droppableId;
+        // add card to the destination list
+        destList.cards.splice(destination.index, 0, movedCard);
+
+        sourceList.cards.forEach((card, idx) => {
+          card.position = idx;
+        });
+
+        // update the order for each card in the destination list
+        destList.cards.forEach((card, idx) => {
+          card.position = idx;
+        });
+
+        setOrderedData(newOrderedData);
+        // TODO: Trigger mutation
+      }
+    }
+  };
+
   return (
-    <DragDropContext onDragEnd={() => {}}>
+    <DragDropContext onDragEnd={onDragEnd}>
       <Droppable droppableId="lists" type="list" direction="horizontal">
         {(provided) => (
           <ol
@@ -29,7 +136,7 @@ export const ListContainer = ({ boardId, data }: ListContainerProps) => {
             ref={provided.innerRef}
             className="flex h-full gap-x-3"
           >
-            {orderedLists.map((list, index) => (
+            {orderedData.map((list, index) => (
               <ListItem key={list.id} index={index} data={list} />
             ))}
             {provided.placeholder}
